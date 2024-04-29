b7dbda8327d81f39df5484f35509ce0941ca0437
Nicolas Guapo <nicolas.lecoeur.guapo@gmail.com>
2024-04-29 11:50:35 +0200

    creation de fichier scss individuelle pour chaque parties

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -78,23 +78,42 @@ console.log(skrollr);
 //initialise la hauteur du body en auto pour s'adapter dynamiquement à la taille de son contenu
 document.body.style.height = "auto";
 
+
+/*CARROUSEL AVEC SWIPERJS*/
+// Crée une nouvelle instance de Swiper et définit les options du carrousel
 const swiper = new Swiper(".swiper-container", {
+  // Effet de transition pour les diapositives du carrousel
   effect: "coverflow",
+  // Affiche une main comme curseur lorsque survolé
   grabCursor: true,
+  // Nombre de diapositives à afficher en même temps (calcul automatique)
   slidesPerView: "auto",
+  // Nombre de diapositives à faire défiler à chaque fois
   slidesPerGroup: 1,
+  // Centre la diapositive active dans le conteneur
   centeredSlides: true,
+  // Espacement entre les diapositives
   spaceBetween: 15,
+  // Crée une boucle infinie pour le carrousel
   loop: true,
+  // Options spécifiques à l'effet coverflow
   coverflowEffect: {
+      // Profondeur des diapositives
       depth: 0,
+      // Modificateur de profondeur
       modifier: 1,
+      // Angle de rotation des diapositives
       rotate: 60,
+      // Échelle des diapositives
       scale: 0.6,
+      // Affichage des ombres des diapositives
       slideShadows: false,
+      // Étirement de la diapositive
       stretch: 0,
   },
+  // Options de lecture automatique
   autoplay: {
+      // Délai entre chaque transition automatique
       delay: 4000,
   },
 });
@@ -111,3 +130,16 @@ burger.addEventListener('click',()=>{
 	footer.classList.toggle('desactive');
 	fullmenu.classList.toggle('visible');
  });
+
+ // GESTION DES LIENS ET APPARITION PAGE PRINCIPALE
+   // Sélection de tous les liens ayant la classe "lien"
+   const liens = document.querySelectorAll('.link');
+   //  Gestionnaire d'événements au clic à chaque lien
+   liens.forEach(lien => {
+		lien.addEventListener('click', () => {
+			main.classList.remove('desactive');
+			footer.classList.remove('desactive');
+			burger.classList.remove('active');
+			fullmenu.classList.remove('visible');
+		 });
+   });
